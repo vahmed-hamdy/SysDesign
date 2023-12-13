7bd4247fb1dda87dd2da5625575f7075800be4c3
Ahmed Hamdy <ahmed.hamdy@ververica.com>
2023-12-13 14:31:23 +0000

    Testing added job

--- FlinkClusterProvider/src/main/java/com/interviews/systemdesign/flinkprovider/controller/HealthController.java
+++ FlinkClusterProvider/src/main/java/com/interviews/systemdesign/flinkprovider/controller/HealthController.java
@@ -7,6 +7,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.Properties;
+
 @RestController
 @RequestMapping("/healthz")
 @Slf4j
@@ -16,5 +22,30 @@ public class HealthController {
     @ResponseStatus(HttpStatus.OK)
     public void announceHealthy(){
         log.debug("I am fine");
+    }
+
+    public static void main(String args[]) throws Exception {
+        Properties props = new Properties();
+        props.setProperty("user", "postgres");
+        props.setProperty("password", "password");
+//        props.setProperty("ssl", "false");
+        try (Connection connection = DriverManager.getConnection("jdbc:postgresql://vvc-db.cxta5ciadexg.eu-central-1.rds.amazonaws.com:5432/vvc_db", props)) {
+            try (Statement statement = connection.createStatement()) {
+//                statement.execute("CREATE TABLE T(\n" +
+//                        "  a INT,\n" +
+//                        "  b VARCHAR(10)\n" +
+//                        ") WITH (\n" +
+//                        "  'connector' = 'filesystem',\n" +
+//                        "  'path' = 'file:///tmp/T.csv',\n" +
+//                        "  'format' = 'csv'\n" +
+//                        ")");
+//                statement.execute("INSERT INTO T VALUES (1, 'Hi'), (2, 'Hello')");
+                try (ResultSet rs = statement.executeQuery("SELECT * FROM vvc_source;")) {
+                    while (rs.next()) {
+                        System.out.println(rs.getString(1) + ", " + rs.getString(2));
+                    }
+                }
+            }
+        }
     }
 }
